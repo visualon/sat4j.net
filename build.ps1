30465f17d1feca0f2a6823cd57af345a13653f71
Michael Kriese <michael.kriese@visualon.de>
2017-06-21 14:20:03 +0200

    Optimized build script.

--- build.ps1
+++ build.ps1
@@ -2,17 +2,24 @@
 
     [string] $ikvmc = "c:\tools\ikvm-8.1.5717.0\bin\ikvmc.exe",
     [string] $version = "2.3.5",
+    [string] $assemblyversion = "2.0.0",
     [string] $pre = $null
 )
 
 New-Item bin -ItemType Directory -ErrorAction SilentlyContinue | Out-Null
 
+if($pre) {
+    $version += "-" + $pre
+}
+
 $ikvm_args = @(
     "-target:library",
-    "-sharedclassloader",
+    "-classloader:ikvm.runtime.AppDomainAssemblyClassLoader",
     "-keyfile:..\featureide.snk",
-    "{", "-version:$version", "..\lib\org.sat4j.core.jar", "}",
-    "{", "-version:$version", "..\lib\org.sat4j.pb.jar", "}"
+    "-version:$assemblyversion",
+    "-fileversion:$version",
+    "{", , "..\lib\org.sat4j.core.jar", "}",
+    "{", "..\lib\org.sat4j.pb.jar", "}"
 )
 
 Write-Output "Compiling jars" | Out-Host
@@ -23,9 +30,6 @@ Pop-Location
 
 Write-Output "Packing files" | Out-Host
 
-if($pre -ne $null) {
-    $version += "-" + $pre
-}
 
-nuget pack org.sat4j.core.nuspec -OutputDirectory bin -version $version $pack
-nuget pack org.sat4j.pb.nuspec -OutputDirectory bin -version $version $pack
+nuget pack org.sat4j.core.nuspec -OutputDirectory bin -version $version
+nuget pack org.sat4j.pb.nuspec -OutputDirectory bin -version $version
