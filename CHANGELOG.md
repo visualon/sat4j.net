5f0231f7bc83e7969454b034ad6a6436d5e8181a
Michael Kriese <michael.kriese@visualon.de>
2020-12-23 15:01:05 +0100

    feat: prepare release (#1)
    * feat: dynamic build
    
    * chore: fix build
    
    * chore: fix build
    
    * feat: prepare release

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -5,11 +5,16 @@ The format is based on [Keep a Changelog](http://keepachangelog.com/)
 and this project adheres to [Semantic Versioning](http://semver.org/).
 
 ## [Unreleased]
+
+## [2.3.6] - 2020-12-23
 ### Changed
 - Updated nuspec file.
+- Release with github actions
+- Update Sat4j to 2.3.6
 
 ## [2.3.5] - 2012-06-21
 ### Added
 - Initial release based on Sat4j 2.3.5
 
-[Unreleased]: https://github.com/ViceIce/unity.wcf/compare/v2.3.5...HEAD
+[Unreleased]: https://github.com/ViceIce/unity.wcf/compare/v2.3.6...HEAD
+[3.5.2]: https://github.com/VisualOn/FeatureIDE.net/compare/v2.3.5...v2.3.6
--- build.ps1
+++ build.ps1
@@ -1,35 +1,55 @@
-﻿param(
+#Requires -Version 5.1
 
-    [string] $ikvmc = "c:\tools\ikvm-8.1.5717.0\bin\ikvmc.exe",
-    [string] $version = "2.3.5",
-    [string] $assemblyversion = "2.0.0",
-    [string] $pre = $null
+param(
+  [string] $version = "2.3.6",
+  [string] $assemblyversion = "2.0.0",
+  [string] $pre = $null
 )
+
+$ErrorActionPreference = 'Stop'
+$ProgressPreference = 'SilentlyContinue'
+$PSDefaultParameterValues['*:ErrorAction'] = 'Stop'
+function ThrowOnNativeFailure {
+  if (-not $?) {
+    throw 'Native Failure'
+  }
+}
 
 New-Item bin -ItemType Directory -ErrorAction SilentlyContinue | Out-Null
 
-if($pre) {
-    $version += "-" + $pre
+$baseUri = "https://repository.ow2.org/nexus/service/local/repositories/releases/content/org/ow2/sat4j"
+
+
+Write-Output "Downloading jars" | Out-Host
+Invoke-WebRequest -URI "$baseUri/org.ow2.sat4j.core/$version/org.ow2.sat4j.core-$version.jar" -OutFile bin/org.sat4j.core.jar
+Invoke-WebRequest -URI "$baseUri/org.ow2.sat4j.pb/$version/org.ow2.sat4j.pb-$version.jar" -OutFile bin/org.sat4j.pb.jar
+
+if ($pre) {
+  $version += "-" + $pre
 }
 
 $ikvm_args = @(
-    "-target:library",
-    "-classloader:ikvm.runtime.AppDomainAssemblyClassLoader",
-    "-keyfile:..\featureide.snk",
-    "-version:$assemblyversion",
-    "-fileversion:$version",
-    "{", , "..\lib\org.sat4j.core.jar", "}",
-    "{", "..\lib\org.sat4j.pb.jar", "}"
+  "-target:library",
+  "-classloader:ikvm.runtime.AppDomainAssemblyClassLoader",
+  "-keyfile:..\featureide.snk",
+  "-version:$assemblyversion",
+  "-fileversion:$version",
+  "{", , "org.sat4j.core.jar", "}",
+  "{", "org.sat4j.pb.jar", "}"
 )
 
 Write-Output "Compiling jars" | Out-Host
 
 Push-Location bin
-& $ikvmc $ikvm_args
+ikvmc $ikvm_args
+ThrowOnNativeFailure
 Pop-Location
 
 Write-Output "Packing files" | Out-Host
 
 
 nuget pack org.sat4j.core.nuspec -OutputDirectory bin -version $version
+ThrowOnNativeFailure
+
 nuget pack org.sat4j.pb.nuspec -OutputDirectory bin -version $version
+ThrowOnNativeFailure
